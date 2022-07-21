0e8c7e29d427e94764f1b3745522d78220beefc8
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-07-21 13:45:43 +0000

    finish5-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,7 +1,24 @@
-class NewController < ApplicationController
+class PostImagesController < ApplicationController
+  def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user_id
+    @post_image.save
+    redirect_to post_image_path
+  end
+
   def show
   end
 
   def index
+  end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
